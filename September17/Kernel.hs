5111ccbd8eec0b1dc947b5e1b7bfb24b5bac2ee7
James Chapman <james.chapman@strath.ac.uk>
2017-09-27 16:43:36 +0100

    beginning again again

---
+++ September17/Kernel.hs
@@ -0,0 +1,41 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE GADTs, DataKinds, TypeOperators, KindSignatures, TypeInType,
+             TypeFamilies #-}
+
+module Kernel where
+
+import Data.Kind
+
+import Utils
+import OPE
+
+data Sort = Chk | Syn | Pnt
+data Kind = Adicity :=> Sort
+type Adicity = [Kind]
+type Scope = Bwd Kind
+
+type family SORT (k :: Kind) :: Sort where
+ SORT (_ :=> s) = s
+
+type family ADICITY (k :: Kind) :: Adicity where
+  ADICITY (ks :=> _) = ks
+
+data Term :: Sort -> Scope -> * where
+  Star :: Term Chk B0
+  Pi   :: (TermK ('[] :=> Chk) >< TermK ('[ '[] :=> Syn] :=> Chk)) gamma ->
+          Term Chk gamma
+  Lam  :: TermK ('[ '[] :=> Syn] :=> Chk) gamma -> Term Chk gamma
+  Emb  :: TermK ('[] :=> Syn) gamma -> Term Chk gamma
+  (:$) :: CoP B0 gamma' gamma -> Spine ks gamma' -> Term s gamma
+  App  :: (TermK ('[] :=> Syn) >< TermK ('[] :=> Chk)) gamma -> Term Syn gamma
+
+data Spine :: [Kind] -> Bwd Kind -> * where
+  S0 :: Spine '[] B0
+  SC :: (TermK k >< Spine ks) gamma -> Spine (k:ks) gamma
+  
+type TermK k = Bind (ADICITY k) (Term (SORT k))
+
+type family Bind (sc :: Adicity)(f :: Bwd Kind -> *) :: (Bwd Kind -> *)
+  where
+  Bind '[] f = f
+  Bind (k:ks) f = k !- Bind ks f
