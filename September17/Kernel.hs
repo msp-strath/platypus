6e7a4d74fbaf17e87b7f47f228e4a661dcf47f85
James Chapman <james.chapman@strath.ac.uk>
2017-09-28 14:41:06 +0100

    some scaffolding for substitution

--- September17/Kernel.hs
+++ September17/Kernel.hs
@@ -1,6 +1,6 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE GADTs, DataKinds, TypeOperators, KindSignatures, TypeInType,
-             TypeFamilies #-}
+             TypeFamilies, ExistentialQuantification, Rank2Types, ScopedTypeVariables #-}
 
 module Kernel where
 
@@ -26,8 +26,8 @@ data Term :: Sort -> Scope -> * where
           Term Chk gamma
   Lam  :: TermK ('[ '[] :=> Syn] :=> Chk) gamma -> Term Chk gamma
   Emb  :: TermK ('[] :=> Syn) gamma -> Term Chk gamma
-  (:$) :: CoP (B0 :< (ks :=> s)) gamma' gamma -> Spine ks gamma' -> Term s gamma
-  App  :: (TermK ('[] :=> Syn) >< TermK ('[] :=> Chk)) gamma -> Term Syn gamma
+  (:$) :: CoP (B0 :< (ks :=> s)) gamma' gamma -> Spine ks gamma' -> Term s gamma -- meta level instantiation
+  App  :: (TermK ('[] :=> Syn) >< TermK ('[] :=> Chk)) gamma -> Term Syn gamma -- object level app
 
 data Spine :: [Kind] -> Bwd Kind -> * where
   S0 :: Spine '[] B0
@@ -40,8 +40,37 @@ type family Bind (sc :: Adicity)(f :: Bwd Kind -> *) :: (Bwd Kind -> *)
   Bind '[] f = f
   Bind (k:ks) f = k !- Bind ks f
 
+data ContextEntry :: Kind -> Scope -> * where
+  CESyn :: Term Chk ^ gamma {- type -}
+        -> ContextEntry ('[] :=> Syn) gamma
+  CEBind :: ContextEntry k gamma -> ContextEntry (ks :=> i) (gamma:<k) ->
+            ContextEntry ((k:ks) :=> i) gamma
+  -- something for Chk?
+  
+data Context :: Scope -> * where
+  C0 :: Context B0
+  CS :: Context gamma -> ContextEntry k gamma -> Context (gamma :< k) 
+{-
+kind and scope indexed context entries
+-}
+data Image :: Scope -> Kind -> * where
+  ISyn :: Term Chk ^ delta -> Image delta ('[] :=> Syn)
+  IBind :: Image (delta:<k) (ks :=> i) -> Image delta ((k:ks) :=> i)
+  -- something for Chk?
+  
+data Mor (gamma :: Scope)(delta :: Scope) :: * where
+  Mor :: Context gamma
+      -> Context delta              
+      -> Riffle rho gamma sigma 
+      -> rho <= delta           
+      -> ALL (Image delta) sigma
+      -> Mor gamma delta
 
+subChk :: Mor gamma delta -> Term Chk gamma -> Term Chk ^ delta -> Term Chk ^ delta
+subChk = undefined
 
+subSyn :: Mor gamma delta -> Term Syn gamma -> (Term Chk ^ delta,Term Chk ^ delta)
+subSyn = undefined
 -- some examples
 
 idFun :: Term Chk B0
